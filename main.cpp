b6a0281657a9ae0a71c37ca93ccf71fec580ee7b
sakarkal <karkalsaad@gmail.com>
2024-06-13 21:22:41 +0100

    pars parameters

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
 #include "Server.hpp"
+void	check_port(string port)
+{
+	for (int a = 0; port[a]; a++)
+	{
+		if (!isdigit(port[a]))
+		{
+			cerr << "\e[033;0;31m	⚡︎   ❌ Error PORT ❌	⚡\e[0m" << endl;
+			exit(1);
+		}
+	}
+}
+
+
 
 int main (int argc, char **argv) {
 
-	if (argc != 3) {
-		cerr << "Usage: ./ircserv <port> <password>" << endl;
+	if (argc == 3)
+	{
+		string	port = argv[1];
+
+		check_port(port);
+
+		Server	srv(atoi(argv[1]), argv[2]);
+
+		srv.start();
+
 	}
-
-	// TODO check for argv validation
-
-	Server srv(6969, argv[2]);
-
-	srv.start();
+	else
+	{
+		cerr << "Usage: ./ircserv <port> <password>" << endl;
+		return 1;
+	}
 
 	return 0;
 }
