76c53df7ed72a76f559ade2a9fe6305aa949354a
sakarkal <karkalsaad@gmail.com>
2024-07-09 22:33:45 +0100

    IRC UPDATES : Adding methode removeOperator

--- include/Channel.hpp
+++ include/Channel.hpp
@@ -55,6 +55,7 @@ public:
 	bool hasPassword() const;
 	bool hasMember(string &nick);
 	void addOperator(Client &client);
+	void removeOperator(Client &client);
 	void broadcastMessageToGroup(string message, map<string, Client*> &group, string senderName);
 	void broadcastAction(Client &client, string reason, BroadCastAction action);
 	bool isOperator(string &nick);
